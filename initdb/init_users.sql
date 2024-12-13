e1f8120df4645fd2f480b24522fcc8f61670c2b4
Timur <timstudiocode@gmail.com>
2024-12-14 01:55:14 +0300

    fix

--- initdb/init_users.sql
+++ initdb/init_users.sql
@@ -1,6 +1,10 @@
-CREATE ROLE readonly_user WITH LOGIN PASSWORD '12345678';
+CREATE USER readonly_user WITH PASSWORD '12345678';
 
+GRANT CONNECT ON DATABASE airport TO readonly_user;
+GRANT USAGE ON SCHEMA public TO readonly_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly_user;
 
 --GRANT SELECT ON airlines TO readonly_user;
 --GRANT SELECT ON destinations TO readonly_user;
